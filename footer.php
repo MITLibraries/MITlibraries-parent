87c5491a44de42f9de69956335ec3faee2f7e2b5
Zach Green <zgreen@mit.edu>
2014-02-06 16:35:44 -0500

    Revert c017abf..2d799ba
    This rolls back to commit c017abfcb49754eaa031f02e41d56eb49cb2fa1d.

--- footer.php
+++ footer.php
@@ -102,33 +102,47 @@
 		</footer>		
 	</div>
 
+
+
 <!-- Load JS in Footer -->
-	<script type="text/javascript">
-	if (typeof jQuery == 'undefined')
-	{
-	    document.write(unescape("%3Cscript src='js/jquery.js' type='text/javascript'%3E%3C/script%3E"));
-	}
-	</script>
-	<script src="<?php bloginfo('template_directory') ?>/libs/bootstrap/js/bootstrap.min.js"></script>
-	<script src="<?php bloginfo('template_directory') ?>/js/core.js"></script>
-	<script src="http://maps.googleapis.com/maps/api/js?sensor=false"></script>
-	<script src="<?php bloginfo('template_directory') ?>/libs/infobox/infobox.js"></script>
-	<script>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js" type="text/javascript"></script>
+<script type="text/javascript">
+if (typeof jQuery == 'undefined')
+{
+    document.write(unescape("%3Cscript src='js/jquery.js' type='text/javascript'%3E%3C/script%3E"));
+}
+</script>
+<script src="<?php bloginfo('template_directory') ?>/libs/datepicker/glDatePicker.min.js"></script>
+<script src="<?php bloginfo('template_directory') ?>/js/jquery.cycle.js"></script>
+<script src="<?php bloginfo('template_directory') ?>/libs/bootstrap/js/bootstrap.min.js"></script>
+<script src="<?php bloginfo('template_directory') ?>/libs/lightbox/js/lightbox.js"></script>
+<script src="<?php bloginfo('template_directory') ?>/js/core.js"></script>
+<script src="http://maps.googleapis.com/maps/api/js?sensor=false"></script>
+<script src="<?php bloginfo('template_directory') ?>/libs/infobox/infobox.js"></script>
+<script>
 
-	// Javascript to enable link to tab
-	var url = document.location.toString();
 
-	if (url.match('#')) {
-	    $('.tabnav a[href=#'+url.split('#')[1]+']').tab('show') ;
-	} 
+$("#hero").cycle({
+	fx: "fade",
+	speed: 1500,
+	pause: 0
 
-	// Change hash for page-reload
-	$('.tabnav a').on('shown', function (e) {
-	    window.location.hash = e.target.hash;
-	})
+});
 
-	</script>
+// Javascript to enable link to tab
+var url = document.location.toString();
 
-	<?php wp_footer(); ?>
-	</body>
+if (url.match('#')) {
+    $('.tabnav a[href=#'+url.split('#')[1]+']').tab('show') ;
+} 
+
+// Change hash for page-reload
+$('.tabnav a').on('shown', function (e) {
+    window.location.hash = e.target.hash;
+})
+
+</script>
+
+<?php	wp_footer(); ?>
+</body>
 </html>
