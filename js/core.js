0a53497ed3390ac4008846c14fa3f535fb1ac0b9
Zach Green <zgreen@mit.edu>
2014-03-05 15:38:38 -0500

    Remove date picker js from core.js, load only on /hours

--- js/core.js
+++ js/core.js
@@ -24,8 +24,6 @@ var Core = {
 		
 		Core.fixSidebar();
 		
-		Core.buildDatePicker();
-		
 		Core.buildMap();
 		
 		Core.nullAlt();
@@ -251,28 +249,6 @@ var Core = {
 	
 	hideMap: function() {
 		$("#map").slideUp(200);
-	},
-	
-	buildDatePicker: function() {
-		$("#hourCalendar").glDatePicker({
-				showAlways: true,
-				selectedDate: todayDate,
-				
-				prevArrow: '<i class="icon-arrow-left"></i>',
-				nextArrow: '<i class="icon-arrow-right"></i>',
-				dowNames: "SMTWTFS",
-				dowOffset: 1,
-				onClick: function(target, cell, date, date2) {
-					var newDate = date.getFullYear()+"-"+(date.getMonth()+1)+"-"+date.getDate();
-					
-					var path = window.location.pathname;
-					var newUrl = path+"?d="+newDate;
-
-					window.location = newUrl;
-				}
-				
-		});
-		
 	},
 	
 	fixSidebar: function() {
