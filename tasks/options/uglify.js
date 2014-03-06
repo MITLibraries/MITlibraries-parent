40e21925ced78b31c33c4c28c64e840e1346fd87
Zach Green <zgreen@mit.edu>
2014-03-06 10:33:45 -0500

    Update ulgify task for hours scripts

--- tasks/options/uglify.js
+++ tasks/options/uglify.js
@@ -2,5 +2,9 @@ module.exports = {
   build: {
     src: 'js/build/production.js',
     dest: 'js/build/production.min.js'
+  },
+  hours: {
+  	src: 'js/build/hours.js',
+  	dest: 'js/build/hours.min.js'
   }
 }
