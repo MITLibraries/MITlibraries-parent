990e8711959c396e5f306b3ec8b9535b312da877
Zach Green <zgreen@mit.edu>
2014-03-06 10:08:00 -0500

    Concat and minify js, then register and enqueue

--- footer.php
+++ footer.php
@@ -112,8 +112,6 @@ if (typeof jQuery == 'undefined')
     document.write(unescape("%3Cscript src='js/jquery.js' type='text/javascript'%3E%3C/script%3E"));
 }
 </script>
-<script src="<?php bloginfo('template_directory') ?>/libs/bootstrap/js/bootstrap.min.js"></script>
-<script src="<?php bloginfo('template_directory') ?>/js/core.js"></script>
 <script>
 
 // Javascript to enable link to tab
