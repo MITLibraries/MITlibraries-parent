9c77679e39ee355bdad6e3c331a8452f6ec8a191
Zach Green <zgreen@mit.edu>
2014-03-20 16:45:48 -0400

    Maintenance message grammar

--- inc/maintenance.php
+++ inc/maintenance.php
@@ -1,5 +1,5 @@
 <div class="siteTitle maintenance group">
-	<h1 class="name-site"><?php bloginfo('name'); ?> <span>Is Under Maintenance</span></h1>
+	<h1 class="name-site"><?php bloginfo('name'); ?> <span>is under maintenance.</span></h1>
 </div>
 <div id="content" class="content maintenance group">
 	<h2 class="message-maintenance">Sorry, this site is currently undergoing some work. Please check back shortly.</h2>
