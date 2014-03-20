eb87086525512aa9d25c478ee2b54456a53c5845
Zach Green <zgreen@mit.edu>
2014-03-20 15:59:01 -0400

    Basic maintenance page

---
+++ inc/maintenance.php
@@ -0,0 +1,6 @@
+<div class="siteTitle maintenance group">
+	<h1 class="siteName"><?php bloginfo('name'); ?> <span>Is Under Maintenance</span></h1>
+</div>
+<div id="content" class="content maintenance group">
+	<h2 class="message-maintenance">Sorry, this site is currently undergoing some work. Please check back shortly.</h2>
+</div>
