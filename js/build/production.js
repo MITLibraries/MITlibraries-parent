9e8b00ec58101842ba1bb48abbe72c0a93340120
Zach Green <zgreen@mit.edu>
2014-03-24 14:05:11 -0400

    Match meganav ga categories and actions to existing
    "link" and "click"

--- js/build/production.js
+++ js/build/production.js
@@ -2386,15 +2386,15 @@ $(function() {
 	$('.ss-nav-menu-item-depth-0 > a').click(function(){
 		var linkHref = $(this).attr('href');
 		var linkText = $(this).text();
-		var linkInfo = 'top_' + linkText + '_' + linkHref;
-		TrackEvent('links', 'mega-nav', linkInfo, 1);
+		var linkInfo = 'mega-nav_top_' + linkText + '_' + linkHref;
+		TrackEvent('link', 'click', linkInfo, 1);
 		console.log('top link clicked');
 	});
 	$('.sub-menu a').click(function(){
 		var linkHref = $(this).attr('href');
 		var linkText = $(this).text();
-		var linkInfo = 'sub_' + linkText + '_' + linkHref;
-		TrackEvent('links', 'mega-nav', linkInfo, 1);
+		var linkInfo = 'mega-nav_sub_' + linkText + '_' + linkHref;
+		TrackEvent('link', 'click', linkInfo, 1);
 		console.log('sub link clicked');
 	});
 	function TrackEvent(Category,Action,Label, Value){
