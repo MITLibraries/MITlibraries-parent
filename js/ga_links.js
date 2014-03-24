f5912b25fdcb84ccd62a4415487edc7ad90c39fb
Zach Green <zgreen@mit.edu>
2014-03-24 14:10:46 -0400

    Remove console.log

--- js/ga_links.js
+++ js/ga_links.js
@@ -4,14 +4,12 @@ $(function() {
 		var linkText = $(this).text();
 		var linkInfo = 'mega-nav_top_' + linkText + '_' + linkHref;
 		TrackEvent('link', 'click', linkInfo, 1);
-		console.log('top link clicked');
 	});
 	$('.sub-menu a').click(function(){
 		var linkHref = $(this).attr('href');
 		var linkText = $(this).text();
 		var linkInfo = 'mega-nav_sub_' + linkText + '_' + linkHref;
 		TrackEvent('link', 'click', linkInfo, 1);
-		console.log('sub link clicked');
 	});
 	function TrackEvent(Category,Action,Label, Value){
 		_gaq.push(['_trackEvent', Category, Action, Label, Value]);
