d62930994547ea28730824484a3ff3b551f1bc89
Zach Green <zgreen@mit.edu>
2014-03-24 13:25:12 -0400

    GA on meganav links

---
+++ js/ga_links.js
@@ -0,0 +1,18 @@
+$(function() {
+	$('.main-navigation .megaMenu a').click(function(){
+		var linkHref = $(this).attr('href');
+		var linkText = $(this).text();
+		var linkInfo = 'top_' + linkText + '_' + linkHref;
+		TrackEvent('links', 'mega-nav', linkInfo, 1);
+	});
+	$('.main-navigation .megaMenu .sub-menu a').click(function(){
+		var linkHref = $(this).attr('href');
+		var linkText = $(this).text();
+		var linkInfo = 'sub_' + linkText + '_' + linkHref;
+		TrackEvent('links', 'mega-nav', linkInfo, 1);
+	});
+	function TrackEvent(Category,Action,Label, Value){
+		_gaq.push(['_trackEvent', Category, Action, Label, Value]);
+	}
+});
+
