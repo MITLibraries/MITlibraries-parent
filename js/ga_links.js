18701f58a1c27de8e43b56a0472cb6eae84f2990
Zach Green <zgreen@mit.edu>
2014-03-24 13:57:04 -0400

    Better selectors for ga links in meganav

--- js/ga_links.js
+++ js/ga_links.js
@@ -1,15 +1,17 @@
 $(function() {
-	$('.main-navigation .megaMenu > li a').click(function(){
+	$('.ss-nav-menu-item-depth-0 > a').click(function(){
 		var linkHref = $(this).attr('href');
 		var linkText = $(this).text();
 		var linkInfo = 'top_' + linkText + '_' + linkHref;
 		TrackEvent('links', 'mega-nav', linkInfo, 1);
+		console.log('top link clicked');
 	});
-	$('.main-navigation .megaMenu .sub-menu a').click(function(){
+	$('.sub-menu a').click(function(){
 		var linkHref = $(this).attr('href');
 		var linkText = $(this).text();
 		var linkInfo = 'sub_' + linkText + '_' + linkHref;
 		TrackEvent('links', 'mega-nav', linkInfo, 1);
+		console.log('sub link clicked');
 	});
 	function TrackEvent(Category,Action,Label, Value){
 		_gaq.push(['_trackEvent', Category, Action, Label, Value]);
