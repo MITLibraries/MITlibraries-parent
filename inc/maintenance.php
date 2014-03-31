a5bd2143d384dd57c59244952ff50a85c20e5429
Zach Green <zgreen@mit.edu>
2014-03-31 11:32:14 -0400

    Update maintenance message

--- inc/maintenance.php
+++ inc/maintenance.php
@@ -2,5 +2,5 @@
 	<h1 class="name-site"><?php bloginfo('name'); ?> <span>is under maintenance.</span></h1>
 </div>
 <div id="content" class="content maintenance group">
-	<h2 class="message-maintenance">Sorry, this site is currently undergoing some work. Please check back shortly.</h2>
+	<h2 class="message-maintenance">Sorry, this site is undergoing a scheduled update between 9am-10am as part of our redesign.</h2>
 </div>
