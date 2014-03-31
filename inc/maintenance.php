1a6ab628a3bb84ae7d9cba3020c1c4ca9effa683
Zach Green <zgreen@mit.edu>
2014-03-31 14:59:20 -0400

    Elaborate maintenance message

--- inc/maintenance.php
+++ inc/maintenance.php
@@ -2,5 +2,8 @@
 	<h1 class="name-site"><?php bloginfo('name'); ?> <span>is under maintenance.</span></h1>
 </div>
 <div id="content" class="content maintenance group">
-	<h2 class="message-maintenance">Sorry, this site is undergoing a scheduled update between 9am-10am as part of our redesign.</h2>
+	<div class="message-maintenance">
+		<h2>Sorry, this site is undergoing a scheduled update between 9am-10am as part of our redesign.</h2>
+		<p>Pages you have bookmarked may need to be updated after the new site is available.</p>
+	</div>
 </div>
