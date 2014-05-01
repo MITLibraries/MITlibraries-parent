713b269e3f80c15e99bee1086e77c5f03ee56a23
Zach Green <zgreen@mit.edu>
2014-05-01 15:35:13 -0400

    Set active class on meganav primary links

--- js/build/production.js
+++ js/build/production.js
@@ -2406,6 +2406,7 @@ $(function(){
   // Toggle "visually-hidden" class
   $('.link-primary').hover(function(){
      $(this).find('.links-sub').toggleClass('visually-hidden');
+     $('>a',this).toggleClass('active');
   });
 
   // Calc nav and div.links-sub width
