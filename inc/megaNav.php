b0da0021a4e0d4c3a52be0c2dfd0d57b5dbb44d1
Zach Green <zgreen@mit.edu>
2014-05-01 11:57:15 -0400

    New meganav markup

--- inc/megaNav.php
+++ inc/megaNav.php
@@ -1,23 +1,60 @@
-<nav id="site-navigation" class="span12 main-navigation" role="navigation">
-	<h3 class="menu-toggle"><?php _e( 'Menu', 'twentytwelve' ); ?></h3>
-	<a class="assistive-text" href="#content" title="<?php esc_attr_e( 'Skip to content', 'twentytwelve' ); ?>"><?php _e( 'Skip to content', 'twentytwelve' ); ?></a>
-	<?php 
-
-		global $blog_id;
-		$current_blog_id = $blog_id;
-
-		//switch to the main blog which will have an id of 1
-		switch_to_blog(1);
-
-		//output the WordPress navigation menu
-		wp_nav_menu(
-			array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu' )
-		);
-
-		//switch back to the current blog being viewed
-		switch_to_blog($current_blog_id); 
-
-	// wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu' ) );
-
-	?>	
-</nav><!-- #site-navigation -->
+<nav class="nav-main">
+  <div id="link-primary" class="link-primary">
+    <a href="/search">Search</a>
+    <div class="links-sub visually-hidden group">
+      <div class="col-1">
+        <span class="heading-col">Start Here</span>
+        <a href="/bartonplus">Articles, books, &amp; more: BartonPlus</a>
+        <a href="/vera">E-journals & databases: Vera </a>
+        <a href="/barton">Books & media: Barton catalog </a>
+        <a href="http://mit.worldcat.org">Non-MIT items: MIT’s WorldCat</a>
+        <a href="/barton-reserves">Course reserves</a>
+      </div>
+      <div class="col-2">
+        <span class="heading-col">Also Try</span>
+        <a href="/fulltext">FullText Finder</a>
+        <a href="http://libguides.mit.edu/content.php?pid=104796&sid=788991">Google Scholar for MIT</a>
+        <a href="/libx">LibX: search/authenticate via browser </a>
+        <a href="http://dspace.mit.edu/">DSpace: MIT research </a>
+        <a href="http://dome.mit.edu/">Dome: MIT-digitized images, maps, etc.</a>
+        <a href="/about/site-search">MIT Libraries web site search</a>
+      </div>
+      <a href="/search" class="bottom">More search options</a>
+    </div><!-- end div.links-sub -->
+  </div><!-- end div.links-primary -->
+  <div id="link-primary" class="link-primary">
+    <a href="/locations">Hours &amp; Locations</a>
+    <div class="links-sub visually-hidden">
+      <div class="col-1"></div>
+      <div class="col-2"></div>
+    </div>
+  </div>
+  <div id="link-primary" class="link-primary">
+    <a href="/borrow">Borrow &amp; Request</a>
+    <div class="links-sub visually-hidden">
+      <div class="col-1"></div>
+      <div class="col-2"></div>
+    </div>
+  </div>
+  <div id="link-primary" class="link-primary">
+    <a href="/research-support">Research Support</a>
+    <div class="links-sub visually-hidden push">
+      <div class="col-1"></div>
+      <div class="col-2"></div>
+    </div>
+  </div>
+  <div id="link-primary" class="link-primary">
+    <a href="/productivity-tools">Productivity Tools</a>
+    <div class="links-sub visually-hidden push">
+      <div class="col-1"></div>
+      <div class="col-2"></div>
+    </div>
+  </div>
+  <div id="link-primary" class="link-primary">
+    <a href="/about">About Us</a>
+    <div class="links-sub visually-hidden push">
+      <div class="col-1"></div>
+      <div class="col-2"></div>
+    </div>
+  </div>
+</nav>
