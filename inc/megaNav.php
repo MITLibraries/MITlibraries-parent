6016fee9aa16071661ea2408e9814a97f14827e8
Zach Green <zgreen@mit.edu>
2014-05-05 14:45:02 -0400

    meganav full HTML markup

--- inc/megaNav.php
+++ inc/megaNav.php
@@ -1,12 +1,12 @@
 <nav class="nav-main border-box group">
-  <div id="link-primary" class="link-primary">
+  <div class="link-primary">
     <a href="/search">Search</a>
     <div class="links-sub group">
       <div class="col-1">
         <span class="heading-col">Start Here</span>
         <a href="/bartonplus">Articles, books, &amp; more: BartonPlus</a>
-        <a href="/vera">E-journals & databases: Vera </a>
-        <a href="/barton">Books & media: Barton catalog </a>
+        <a href="/vera">E-journals &amp; databases: Vera </a>
+        <a href="/barton">Books &amp; media: Barton catalog </a>
         <a href="http://mit.worldcat.org">Non-MIT items: MIT’s WorldCat</a>
         <a href="/barton-reserves">Course reserves</a>
       </div>
@@ -22,39 +22,113 @@
       <a href="/search" class="bottom">More search options</a>
     </div><!-- end div.links-sub -->
   </div><!-- end div.links-primary -->
-  <div id="link-primary" class="link-primary">
+  <div class="link-primary">
     <a href="/locations">Hours &amp; Locations</a>
     <div class="links-sub">
-      <div class="col-1"></div>
-      <div class="col-2"></div>
+      <div class="col-1">
+        <span class="heading-col">Locations</span>
+        <a href="/barker">Barker Library</a>
+        <a href="/dewey">Dewey Library</a>
+        <a href="/hayden">Hayden Library</a>
+        <a href="/archives">Institute Archives &amp; Special Collections</a>
+        <a href="/lewis">Lewis Music Library</a>
+        <a href="/lsa">Library Storage Annex</a>
+        <a href="/rotch">Rotch Library</a>
+      </div>
+      <div class="col-2">
+        <span class="heading-col">Using The Libraries</span>
+        <a href="/study">Study spaces</a>
+        <a href="/copying">Scan, copy, print</a>
+        <a href="/exhibits">Exhibits &amp; Galleries</a>
+        <a href="/vistors">Non-MIT visitors</a>
+        <a href="/about/guidelines">Guidelines for using the MIT Libraries</a>
+      </div>
+      <a href="/hours" class="bottom">All Hours</a>
+      <a href="/locations" class="bottom">All Locations</a>
+      <a href="/locations/map" class="bottom">Map</a>
     </div>
   </div>
-  <div id="link-primary" class="link-primary">
+  <div class="link-primary">
     <a href="/borrow">Borrow &amp; Request</a>
     <div class="links-sub">
-      <div class="col-1"></div>
-      <div class="col-2"></div>
+      <div class="col-1">
+        <span class="heading-col">Borrow, Request, Renew</span>
+        <a href="/barton-account">Your Account: renew MIT, BLC &amp; Borrow Direct items</a>
+        <a href="http://library.mit.edu">Barton catalog: get MIT items</a>
+        <a href="http://mit.worldcat.org">MIT’s WorldCat: get non-MIT items (BLC &amp; Borrow Direct)</a>
+        <a href="/illiad">ILLiad: get non-MIT items (Interlibrary Borrowing)</a>
+        <a href="/suggest-purchase">Suggest a purchase</a>
+      </div>
+      <div class="col-2">
+        <span class="heading-col">More Information</span>
+        <a href="/getit">Get it: understand your options</a>
+        <a href="http://libguides.mit.edu/circfaq">Circulation FAQ: fines, renewing, etc.</a>
+        <a href="http://libguides.mit.edu/reserves">Course reserves &amp; TIP FAQ</a>
+        <a href="/ordering/non-mit-access/index.html">Visit non-MIT libraries: Harvard, Boston Library Consortium, etc.</a>
+      </div>
+      <a href="/borrow" class="bottom">More borrow &amp; request options</a>
     </div>
   </div>
-  <div id="link-primary" class="link-primary">
+  <div class="link-primary">
     <a href="/research-support">Research Support</a>
     <div class="links-sub">
-      <div class="col-1"></div>
-      <div class="col-2"></div>
+      <div class="col-1">
+        <span class="heading-col">Expert Help</span>
+        <a href="/ask">Ask Us!</a>
+        <a href="http://libguides.mit.edu/content.php?pid=110460&sid=1651114">Librarians &amp; subject experts</a>
+        <a href="/research-guides">Research guides</a>
+        <a href="http://libguides.mit.edu/classguides">Class &amp; program guides</a>
+        <a href="http://libguides.mit.edu/finding-data">Data: finding &amp; managing</a>
+        <a href="/research-support/connect/">Connect from on and off-campus</a>
+      </div>
+      <div class="col-2">
+        <span class="heading-col">Publishing &amp; Writing</span>
+        <a href="/scholarly">Scholarly publishing: open access &amp; copyright</a>
+        <a href="http://dspace.mit.edu/">DSpace@MIT: MIT scholarly papers</a>
+        <a href="/oapolicy">Faculty open access policy</a>
+        <a href="http://libguides.mit.edu/citing">Citing sources</a>
+        <a href="http://libguides.mit.edu/content.php?pid=42346&sid=311602">Getting published: tools &amp; help</a>
+        <a href="/archives/thesis-specs/index.html">Thesis specifications</a>
+      </div>
+      <a href="/research-support" class="bottom">More research support options</a>
     </div>
   </div>
-  <div id="link-primary" class="link-primary">
+  <div class="link-primary">
     <a href="/productivity-tools">Productivity Tools</a>
     <div class="links-sub push">
-      <div class="col-1"></div>
-      <div class="col-2"></div>
+      <div class="col-1">
+        <span class="heading-col">Web Tools &mp; Apps</span>
+        <a href="/apps">Apps for academics</a>
+        <a href="/fulltext">FullText Finder: find specific citation</a>
+        <a href="/libx">LibX: search/authenticate via browser</a>
+        <a href="/mobile">Mobile site</a>
+        <a href="/new-books">New books RSS feeds</a>
+      </div>
+      <div class="col-2">
+        <span class="heading-col">Citation &amp; Content Management Tools</span>
+        <a href="/references">Citation software: EndNote, RefWorks, Zotero, Mendeley</a>
+        <a href="http://libguides.mit.edu/manage-info">Manage your information</a>
+      </div>
+      <a href="http://libraries.mit.edu/productivity-tools" class="bottom">More productivity tools</a>
     </div>
   </div>
-  <div id="link-primary" class="link-primary">
+  <div class="link-primary">
     <a href="/about">About Us</a>
     <div class="links-sub push">
-      <div class="col-1"></div>
-      <div class="col-2"></div>
+      <div class="col-1">
+        <span class="heading-col">About Us</span>
+        <a href="http://libguides.mit.edu/directory">Staff directory</a>
+        <a href="/about/guidelines.html">Guidelines for using the MIT Libraries</a>
+        <a href="/about/#Jobs">Jobs</a>
+        <a href="/giving">Giving to the MIT Libraries</a>
+      </div>
+      <div class="col-2">
+        <span class="heading-col">News, Events, &amp; Exhibits</span>
+        <a href="/calendar">Calendar of events</a>
+        <a href="/news">News</a>
+        <a href="/exhibits">Exhibits &amp; galleries</a>
+      </div>
+      <a href="/about" class="bottom">More about us</a>
     </div>
   </div>
 </nav>
