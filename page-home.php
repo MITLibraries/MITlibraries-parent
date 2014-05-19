eb540cbab8ba7d24a633ea57ed4ebd66b767afff
Zach Green <zgreen@mit.edu>
2014-05-19 16:50:57 -0400

    define h2s on main homepage content areas

--- page-home.php
+++ page-home.php
@@ -4,9 +4,15 @@
 
 <body <?php body_class(); ?>>
 	<div class="search--resources"></div>
-	<div class="hours-locations"></div>
-	<div class="news-events"></div>
-	<div class="guides-experts"></div>
+	<div class="hours-locations">
+		<h2>Hours &amp; Locations</h2>
+	</div>
+	<div class="news-events">
+		<h2>News &amp; Events</h2>
+	</div>
+	<div class="guides-experts">
+		<h2>Research Guides &amp; Experts</h2>
+	</div>
 </body>
 
 <?php 
