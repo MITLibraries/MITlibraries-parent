d0127fcff46ececa738c4cf6b1a73959b1635cb2
Zach Green <zgreen@mit.edu>
2014-05-19 16:36:02 -0400

    Revert "Dummy homepage content"
    This reverts commit c0da7f7cb0d052961a487fcc76985023214cf3ea.

--- page-home.php
+++ page-home.php
@@ -3,21 +3,6 @@
 ?>
 
 <body <?php body_class(); ?>>
-	<h1>testing h1</h1>
-	<h2>testing h2</h2>
-	<h3>testing h3</h3>
-	<h4>testing h4</h4>
-	<h5>testing h5</h5>
-	<h6>testing h6</h6>
-	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta, numquam, deserunt, ex adipisci aperiam vero consequuntur dignissimos tempore optio reprehenderit officia dolore ea laborum molestias quibusdam provident excepturi blanditiis sequi.</p>
-	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Modi, nostrum, ex, suscipit amet non consequuntur facere voluptatibus quae ab libero sequi necessitatibus molestias voluptas. Officia corporis ab earum excepturi reprehenderit.</p>
-	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil, rerum, ad neque porro soluta voluptate incidunt vel eos omnis dignissimos excepturi odit nisi alias exercitationem temporibus magnam provident ab sequi.</p>
-	<ul>
-		<li>list item</li>
-		<li>list item</li>
-		<li>list item</li>
-		<li>list item</li>
-	</ul>
 </body>
 
 <?php 
