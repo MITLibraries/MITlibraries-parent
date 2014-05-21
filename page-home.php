ece4e3ab6ce0791f09487e5d37232ec9c6499487
Zach Green <zgreen@mit.edu>
2014-05-21 15:25:17 -0400

    Dummy homepage search

--- page-home.php
+++ page-home.php
@@ -2,7 +2,26 @@
 	get_header();
 ?>
 
-	<div class="search--resources"></div>
+	<div class="search--resources">
+		<h2>Search in</h2>
+		<select name="" id="">
+			<option value="">Option #1</option>
+			<option value="">Option #2</option>
+			<option value="">Option #3</option>
+			<option value="">Option #4</option>
+			<option value="">Option #5</option>
+		</select>
+		<label>for</label>
+		<input type="text">
+		<label>by</label>
+		<select name="" id="">
+			<option value="">Option #1</option>
+			<option value="">Option #2</option>
+			<option value="">Option #3</option>
+			<option value="">Option #4</option>
+			<option value="">Option #5</option>
+		</select>
+	</div>
 	<div class="hours-locations">
 		<h2>Hours &amp; Locations</h2>
 		<?php get_template_part('inc/location', 'info'); ?>
