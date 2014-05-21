f70858814d4c6f8debda5a22c04360788a7d4d65
Zach Green <zgreen@mit.edu>
2014-05-21 13:41:40 -0400

    Basic locations WP_Query for homepage

---
+++ inc/location-info.php
@@ -0,0 +1,21 @@
+<?php
+
+	// Query the locations
+	$args = array(
+						'post_type' => 'location',
+						'posts_per_page' => 6,
+						'orderby' => 'menu_order',
+						'order' => 'ASC',
+						'no_found_rows' => true,
+						'update_post_term_cache' => false,
+						'update_post_meta_cache' => false
+					);
+
+	$locationsQuery = new WP_Query($args);
+
+	while ($locationsQuery->have_posts()) {
+			$locationsQuery->the_post();
+			echo '<div class="location"><h3>' . get_the_title() . '</h3></div>';
+	}
+
+?>
