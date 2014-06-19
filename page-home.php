58bc9560875c5144f13f7594a81103a679bc2973
Zach Green <zgreen@mit.edu>
2014-06-19 11:49:59 -0400

    add expert names from JSON

--- page-home.php
+++ page-home.php
@@ -123,6 +123,11 @@
 					  }
 					  if(!found)arr[arr.length]=randomNumber;
 					}
+					var expertName1 = data[arr[0]].title;
+					var expertName2 = data[arr[1]].title;
+					var expertName3 = data[arr[2]].title;
+					var expertName4 = data[arr[3]].title;
+
 					var expertPhoto1 = data[arr[0]].featured_image.guid;
 					var expertPhoto2 = data[arr[1]].featured_image.guid;
 					var expertPhoto3 = data[arr[2]].featured_image.guid;
@@ -141,6 +146,13 @@
 					// var expertPhoto2 = data[randomImage2].featured_image.guid;
 					// Append expert image only if JSON request successful
 					$('.expert').append('<img class="expert-photo">');
+					// Append empty spans for expert names
+					$('.expert').append('<span class="name"></span>');
+					// Add expert name to appropriate span
+					$('.expert .name:eq(0)').text(expertName1);
+					$('.expert .name:eq(1)').text(expertName2);
+					$('.expert .name:eq(2)').text(expertName3);
+					$('.expert .name:eq(3)').text(expertName4);
 					// Add image URL to src attribute
 					$('.expert .expert-photo:eq(0)').attr('src', expertPhoto1);
 					$('.expert .expert-photo:eq(1)').attr('src', expertPhoto2);
