5055f8cafd6b082501990e4270f840ee4d3a9fce
Zach Green <zgreen@mit.edu>
2014-07-28 12:20:44 -0400

    simpler check for homepage news category

--- js/home.js
+++ js/home.js
@@ -378,24 +378,30 @@ $.getJSON('/news/wp-json/posts?type[]=post&type[]=event')
 		if (item1Cat == 'post') {
 			// Append the the Post Type
 			$('.item-1 .category-post').text('News');
-			// Append the post title, trigger an event
-			$('.item-1 h3').append(newsItem1.title).trigger('newsLoaded1');
-			// Get the image attached to the post
-			var newsImage1 = '/news/files/'+newsItem1.featured_image.attachment_meta.file;
-			// Set the image as a background
-			$('.item-1 .image').css('background-image', 'url('+newsImage1+')');
 		}
+		else {
+			$('.item-1 .category-post').text('Event');
+		}
+		// Append the post title, trigger an event
+		$('.item-1 h3').append(newsItem1.title).trigger('newsLoaded1');
+		// Get the image attached to the post
+		var newsImage1 = '/news/files/'+newsItem1.featured_image.attachment_meta.file;
+		// Set the image as a background
+		$('.item-1 .image').css('background-image', 'url('+newsImage1+')');
 		// Check the Post Type
 		if (item2Cat == 'post') {
 			// Append the the Post Type
 			$('.item-2 .category-post').text('News');
-			// Append the post title, trigger an event
-			$('.item-2 h3').append(newsItem2.title).trigger('newsLoaded2');
-			// Get the image attached to the post
-			var newsImage2 = '/news/files/'+newsItem2.featured_image.attachment_meta.file;
-			// Set the image as a background
-			$('.item-2 .image').css('background-image', 'url('+newsImage2+')');
-		}	
+		}
+		else {
+			$('.item-2 .category-post').text('Event');
+		}
+		// Append the post title, trigger an event
+		$('.item-2 h3').append(newsItem2.title).trigger('newsLoaded2');
+		// Get the image attached to the post
+		var newsImage2 = '/news/files/'+newsItem2.featured_image.attachment_meta.file;
+		// Set the image as a background
+		$('.item-2 .image').css('background-image', 'url('+newsImage2+')');
 	})
 	.fail(function(){
 		$('.news-events > .flex-container')
