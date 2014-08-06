c9e65b74d5cb8c60f07cbf8a779e8e8034740180
Zach Green <zgreen@mit.edu>
2014-08-06 13:22:44 -0400

    sentence case fixes

--- page-home.php
+++ page-home.php
@@ -166,7 +166,7 @@
 						<h3><a class="name-location" href="/rotch">Rotch Library</a></h3><div class="hours"></div><div class="location-info"><a href="/map" class="map-location">7-238</a><a href="tel:617-258-5592" class="phone"><span class="number">617-258-5592</span></a></div>
 					</div>
 				</div>
-				<a href="/hours" class="button-primary">All Hours &amp; Locations</a>
+				<a href="/hours" class="button-primary">All hours &amp; locations</a>
 				<div class="extra">
 					<a href="/map" class="button-tertiary link-map more"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="16px" height="16px" viewBox="0 0 16 16" enable-background="new 0 0 16 16" xml:space="preserve"><path d="M16 2.922v12.695c0 0.211-0.117 0.336-0.273 0.336 -0.055 0-0.117-0.016-0.18-0.039l-4.344-2.109c-0.125-0.055-0.281-0.086-0.438-0.086 -0.172 0-0.336 0.031-0.461 0.094l-4.109 2.086C6.062 15.969 5.883 16 5.711 16c-0.156 0-0.305-0.023-0.422-0.07l-4.828-2.141C0.203 13.68 0 13.359 0 13.078V0.383c0-0.219 0.117-0.344 0.289-0.344 0.055 0 0.109 0.008 0.172 0.031l4.828 2.141c0.117 0.047 0.266 0.078 0.422 0.078 0.172 0 0.352-0.039 0.484-0.102l4.109-2.086C10.43 0.039 10.594 0 10.766 0c0.156 0 0.312 0.031 0.438 0.086l4.344 2.109C15.797 2.312 16 2.641 16 2.922zM5.5 14.805V3.484L1 1.523v11.32L5.5 14.805zM10.5 12.508V1.242L6 3.492v11.266L10.5 12.508zM15 3.133l-4-1.906v11.289l4 1.898V3.133z"/></svg> View map</a>
 					<a href="/study" class="button-tertiary link-study more"><svg version="1.1" xmlns="http://www.w3.org/2000/svg" x="0" y="0" width="14" height="16" viewBox="0 0 14 16" enable-background="new 0 0 14.001 16" xml:space="preserve"><path d="M13.8 10.2C14.2 10.6 14 11 13.4 11H6c-1.1 0-2-0.9-2-2H1v6.5C1 15.8 0.8 16 0.5 16S0 15.8 0 15.5v-15C0 0.2 0.2 0 0.5 0S1 0.2 1 0.5V1h5.5c1.1 0 2 0.9 2 2h4.9c0.6 0 0.8 0.4 0.4 0.8l-1.9 2.4c-0.3 0.4-0.3 1.2 0 1.6L13.8 10.2zM7.5 8V3c0-0.6-0.4-1-1-1H1v6H7.5zM12.4 10L11.2 8.4c-0.6-0.8-0.6-2 0-2.8L12.4 4H9.5h-1v5H5c0 0.6 0.4 1 1 1H12.4z"/></svg> Find a study space</a>
@@ -232,7 +232,7 @@
 						switch_to_blog(1);
 					?>
 				</div>
-				<a href="/news" class="button-primary">All News &amp; Events</a>
+				<a href="/news" class="button-primary">All news &amp; events</a>
 			</div><!-- end div.news-events -->
 			<div class="guides-experts">
 				<h2>Research guides &amp; experts</h2>
