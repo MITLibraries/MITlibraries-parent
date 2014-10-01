30467f3197341e2614d5525eb02ca3552b8c3a1b
Zach Green <zgreen@mit.edu>
2014-10-01 08:18:02 -0400

    clean up hours-today.js

--- js/hours-today.js
+++ js/hours-today.js
@@ -2,7 +2,9 @@
 $(function(){
 	var locSelector = $('[data-location-hours]'),
 			apptOnlyLoc = ['Document Services', 'Library Storage Annex'],
-			apptOnly;
+			apptOnly,
+			day,
+			addDay;
 	// If the selector exists
 	if (locSelector.length) {
 		// Today
@@ -14,9 +16,9 @@ $(function(){
 		today = m + "/" + d + "/" + yyyy;
 
 		// Day of week
-		var day = moment().format('dddd').toLowerCase(); // to match JSON
-		
-		var addDay = moment().add(1, 'days').format('M/DD/YYYY');
+		day = moment().format('dddd').toLowerCase(); // to match JSON
+		// Add a day
+		addDay = moment().add(1, 'days').format('M/DD/YYYY');
 
 		$.ajax({
 				cache: false,
@@ -25,49 +27,54 @@ $(function(){
 			})
 			.done(function(json) {
 				// The terms
-				var terms = json.terms;
+				var terms = json.terms,
+						theTerm,
+						locations;
 
 				for (var i = 0; i < terms.length; i++) {
-					// Check each term for start/end dates
-					var termStart = terms[i].termStart;
-					var termEnd = terms[i].termEnd;
-					// Check the range (using Moment.js w/ Twix plugin)
-					var termActive = moment.twix(termStart, termEnd, {parseStrict: true}).isCurrent();
+							// Check each term for start/end dates
+					var termStart = terms[i].termStart,
+							termEnd = terms[i].termEnd,
+							// Check the range (using Moment.js w/ Twix plugin)
+							termActive = moment.twix(termStart, termEnd, {parseStrict: true}).isCurrent();
 					// Set the active term
 					if (termActive === true) {
-						var theTerm = terms[i];
+						theTerm = terms[i];
 					}
 				};
 				// Active term locations
-				var locations = theTerm.locations;
+				locations = theTerm.locations;
 				// Each location
 				locSelector.each(function(){
 
-					var location = $(this).data('location-hours');
-
-					// The location
-					var locationHrs = _.findWhere(theTerm.locations, {"locationName" : location});
-					// Empty exceptions arr for each location
-					var exceptionsArr = [];
-					// Find the first exception that matches today's date
-					var exceptions = _.findWhere(locationHrs.exceptions, {"date" : today});
+					var location = $(this).data('location-hours'),
+							// Location hours
+							locationHrs = _.findWhere(theTerm.locations, {"locationName" : location}),
+							// Empty exceptions arr for each location
+							exceptionsArr = [],
+							// Find the first exception that matches today's date
+							exceptions = _.findWhere(locationHrs.exceptions, {"date" : today}),
+							todayOpen,
+							todayClosed,
+							hoursTodayCompiled,
+							hoursTodayTemplate;
 					// Set the open/closed hours if there is an exception...
 					if (exceptions != undefined) {
-						var todayOpen = '<%= '+'"'+exceptions.open+'"'+' %>';
-						var todayClosed = '<%= '+'"'+exceptions.closed+'"'+' %>';
+						todayOpen = '<%= '+'"'+exceptions.open+'"'+' %>';
+						todayClosed = '<%= '+'"'+exceptions.closed+'"'+' %>';
 					}
 					// or else use today's default hours
 					else {
-						var todayOpen = '<%= '+day+'.open %>';
-						var todayClosed = '<%= '+day+'.closed %>';
+						todayOpen = '<%= '+day+'.open %>';
+						todayClosed = '<%= '+day+'.closed %>';
 					}
 
 					// The template for today's hours
-					var hoursTodayCompiled = _.template(
+					hoursTodayCompiled = _.template(
 							todayOpen + '-' + todayClosed
 					);
 
-					var hoursTodayTemplate = hoursTodayCompiled(locationHrs);
+					hoursTodayTemplate = hoursTodayCompiled(locationHrs);
 
 					$(this).append(hoursTodayTemplate.replace(/:00/g,"").replace(/12am/g,"midnight"));
 
