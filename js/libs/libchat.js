31313428e297c63c404a032b79e34ab04f733a23
Jeremy Prevost <JPrevost@users.noreply.github.com>
2015-06-11 15:59:21 -0400

    Allows chat to work over either http or https
    This addresses a bug in which some browsers don't allow content to be loaded over http when accessed over https...hopefully without side effects, but it seems fairly safe.

--- js/libs/libchat.js
+++ js/libs/libchat.js
@@ -1,4 +1,4 @@
 $(function(){
    var libchat_efc75c55947db323d4faab725b79307f = { iid:59, key:'fb10446730e271c', width:'240' }
-   $.getScript('http://libanswers.mit.edu/load_chat.php?hash=efc75c55947db323d4faab725b79307f&options=libchat_efc75c55947db323d4faab725b79307f');
+   $.getScript('//mit.libanswers.com/load_chat.php?hash=efc75c55947db323d4faab725b79307f&options=libchat_efc75c55947db323d4faab725b79307f');
 });
