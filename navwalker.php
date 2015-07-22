f84690210256300160835bc4089b5e90b67a0bff
pbruk <phillip.bruk@gmail.com>
2015-07-22 14:23:45 -0400

    Adjust code to allow parent links to work

--- navwalker.php
+++ navwalker.php
@@ -82,7 +82,7 @@ class navwalker extends Walker_Nav_Menu {
 
 			// If item has_children add atts to a.
 			if ( $args->has_children && $depth === 0 ) {
-				$atts['href'] = 		'#';
+				$atts['href'] = ! empty( $item->url ) ? $item->url : ''; // new line
 				$atts['data-toggle']	= 'dropdown';
 				$atts['class']			= 'dropdown-toggle';
 				$atts['aria-haspopup']	= 'true';
