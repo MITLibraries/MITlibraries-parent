5498484f9cd87da8ea92f4761e069c58dd5d1a09
pbruk <phillip.bruk@gmail.com>
2015-07-22 14:22:38 -0400

    Add template for proper former selfTitle layout

---
+++ inc/self-title.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Template Name: Self-Title
+ *
+ * Unline other pages in the Libraries theme,
+ * this page template renders the page title
+ * from the post, rather than from the parent
+ * if the page is a child page.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twelve
+ * @since Twenty Twelve 1.0
+ */
+ 
+$pageRoot = getRoot($post);
+$section = get_post($pageRoot);
+$isRoot = $section->ID == $post->ID;
+?>
+
+				<div class="title-page">
+					<h1><?php the_title(); ?></h1>
+				</div>
