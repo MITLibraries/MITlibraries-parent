1dbbfaa914630231695046670f0c4333674cfb2b
Frances Botsford <frances@switchingprotocols.com>
2016-04-06 16:42:05 -0400

    test fixes

--- functions.php
+++ functions.php
@@ -836,16 +836,16 @@ return $mimes;
 }
 add_filter( 'upload_mimes', 'cc_mime_types' ); 
 
-/*
+/**
  * Force URLs in srcset attributes into HTTPS scheme.
- * @link: https://wordpress.org/support/topic/responsive-images-src-url-is-https-srcset-url-is-http-no-images-loaded?replies=19#post-7767555
+ * @link https://wordpress.org/support/topic/responsive-images-src-url-is-https-srcset-url-is-http-no-images-loaded?replies=19#post-7767555
  */
 function ssl_srcset( $sources ) {
-  foreach ( $sources as &$source ) {
-    $source['url'] = set_url_scheme( $source['url'], 'https' );
-  }
+	foreach ( $sources as &$source ) {
+		$source['url'] = set_url_scheme( $source['url'], 'https' );
+	}
 
-  return $sources;
+	return $sources;
 }
 add_filter( 'wp_calculate_image_srcset', 'ssl_srcset' );
 
