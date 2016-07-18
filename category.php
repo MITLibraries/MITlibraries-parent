252e65e86f45c660cfc5431db49df0082fae3174
Matt Bernhardt <mjbernha@mit.edu>
2016-07-18 10:06:41 -0400

    Automated cleaning of Generic.Files.LineEndings

--- category.php
+++ category.php
@@ -12,27 +12,27 @@
 
 get_header(); ?>
 
-	<div id="stage" class="inner" role="main">
-		<div id="content" class="content has-sidebar">
+    <div id="stage" class="inner" role="main">
+        <div id="content" class="content has-sidebar">
 
-				<div class="content-main main-content">			
+                <div class="content-main main-content">         
 		<?php if ( have_posts() ) : ?>
-			<header class="archive-header">
+            <header class="archive-header">
 				<h1 class="archive-title"><?php printf( __( 'Category Archives: %s', 'twentytwelve' ), '<span>' . single_cat_title( '', false ) . '</span>' ); ?></h1>
 
 			<?php if ( category_description() ) : // Show an optional category description ?>
 				<div class="archive-meta"><?php echo category_description(); ?></div>
 			<?php endif; ?>
-			</header><!-- .archive-header -->
+            </header><!-- .archive-header -->
 
 			<?php
 			/* Start the Loop */
 			while ( have_posts() ) : the_post();
 
 				/*
-				 * Include the post format-specific template for the content. If you want to
-				 * this in a child theme then include a file called called content-___.php
-				 * (where ___ is the post format) and that will be used instead.
+                 * Include the post format-specific template for the content. If you want to
+                 * this in a child theme then include a file called called content-___.php
+                 * (where ___ is the post format) and that will be used instead.
 				 */
 				get_template_part( 'content', get_post_format() );
 
@@ -45,10 +45,10 @@ get_header(); ?>
 			<?php get_template_part( 'content', 'none' ); ?>
 		<?php endif; ?>
 
-				</div>
+                </div>
 <?php get_sidebar(); ?>
 
-		</div><!-- #content -->
-	</div>
+        </div><!-- #content -->
+    </div>
 
 <?php get_footer(); ?>
--- inc/breadcrumbs.php
+++ inc/breadcrumbs.php
@@ -8,6 +8,6 @@
 
 ?>
 <div class="breadcrumbs--better hidden-phone group" role="navigation" aria-label="breadcrumbs">
-	<span><a href="/">Libraries home</a></span>
+    <span><a href="/">Libraries home</a></span>
 	<?php better_breadcrumbs(); ?>
 </div>
