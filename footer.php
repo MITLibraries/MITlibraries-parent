e5664d9e3d5a36a4567c60332060520a645ba9e6
Frances Botsford <frances@switchingprotocols.com>
2017-06-07 13:20:36 -0400

    a11y menu access

--- footer.php
+++ footer.php
@@ -132,6 +132,49 @@
 	});
 </script>
 
+<script type="text/javascript">
+
+	$( '.link-primary' ).bind( "mouseenter", function(e) {	
+		$( '.link-primary' ).removeClass( 'open' );
+		$(this).find( '.main-nav-link' ).attr('aria-expanded', 'true');
+		$(this).closest( '.link-primary' ).addClass( 'open' );
+	});
+	$( '.link-primary' ).bind( "mouseleave", function(e) {	
+		$(this).find( '.main-nav-link' ).attr('aria-expanded', 'false');
+		$( '.link-primary' ).removeClass( 'open' );
+	});
+
+
+	$('#nav-main').keydown(function(e) {
+        var $focused = document.activeElement;
+
+        switch(e.keyCode)
+        {
+            // user presses the "space"
+            case 32:    showMenu(e, $focused);
+                        break;
+
+            // user presses the "esc" key
+            case 27:    hideMenu(e,);
+                        break;
+        }
+    });
+
+	/* show/hide the menu on click */
+  function showMenu(e, focused) {
+  	$( '.link-primary' ).removeClass( 'open' );
+    $(focused).closest( '.link-primary' ).addClass( 'open' );
+    $(focused).attr('aria-expanded', 'true');
+    event.preventDefault();
+    event.stopPropagation();
+  }
+  function hideMenu(e) {
+    $( '.link-primary' ).removeClass( 'open' );
+    $( '.main-nav-link' ).attr('aria-expanded', 'false');
+  }
+
+</script>
+
 <?php wp_footer(); ?>
 </body>
 </html>
