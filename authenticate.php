db055a3a82b5bb0c3f744d304f9c5d11e6a283f5
hllavina <hllavina@gmail.com>
2017-11-02 15:38:38 -0400

    Added authenticate form

---
+++ authenticate.php
@@ -0,0 +1,69 @@
+<script type="text/javascript" src="/wp-content/themes/libraries/js/login_functions.js"></script>
+<script type="text/javascript">
+function getCookie(name)
+{
+var i,x,y,ARRcookies = document.cookie.split(";");
+for (i = 0;i < ARRcookies.length;i++)
+{
+  x = ARRcookies[i].substr(0,ARRcookies[i].indexOf("="));
+  y = ARRcookies[i].substr(ARRcookies[i].indexOf("=")+1);
+  x = x.replace(/^\s+|\s+$/g,"");
+  if (x == name)
+    {
+    return unescape(y);
+    }
+  }
+}
+
+	var referer = document.referer;
+	var redirecTo = "";
+
+	$(document).ready(function() {
+	    if (referer != "" && referer != "undefined") {
+			start = location.href.indexOf("pid");
+	        newlocation = location.href.substr(start);
+			redirecTo = newlocation.substr(newlocation.indexOf("=")+1);
+            eppn = '<!--#echo var="eppn" -->';
+        	loginFunctions.doAuthenticate(eppn, redirecTo);
+	     }
+	});
+</script>
+
+<style type="text/css">
+	div.loading {<br />
+	    display:block;<br />
+            width:100%;<br />
+	    height:100%;<br />
+	    margin-left:auto;<br />
+	    margin-right:auto;<br />
+            vertical-align:middle;<br />
+	    text-align:center;<br />
+	}<br />
+	div.inner {<br />
+	    display:block;<br />
+            border:thin solid #659EC7;<br />
+	    margin-top:20%;<br />
+	    margin-left:auto;<br />
+	    margin-right:auto;<br />
+            width:33%;<br />
+	    padding:1em;<br />
+            font-family: 'verdana', 'sans serif', 'helvetica';<br />
+	    font-weight:600;<br />
+            font-size:.75em;<br />
+	    color:#659EC7;<br />
+	}<br />
+	img.loading {<br />
+	    display:block;<br />
+	    clear:both;<br />
+            top:1%;<br />
+	    margin-left:auto;<br />
+	    margin-right:auto;<br />
+	}<br />
+    </style>
+
+&nbsp;
+<div class="loading" align="center">
+<div class="inner">Loading,
+please wait . . .
+<img class="loading" src="/images/load.gif" /></div>
+</div>
