d7e1ead06dbbf45a4a4a799df299e6e4b4d62410
Matt Bernhardt <mjbernha@mit.edu>
2017-12-28 14:04:14 -0500

    Converts  to use get_site_url() in functions.php

--- functions.php
+++ functions.php
@@ -169,13 +169,13 @@ function twentytwelve_scripts_styles() {
 
 	wp_register_script( 'term-hours', get_template_directory_uri() . '/js/build/term-hours.min.js', array( 'jquery', 'productionJS' ), false, true );
 
-	wp_register_script( 'moment', '//' . $_SERVER['SERVER_NAME'] . '/app/libhours/js/vendor/moment.js', false, false, true );
+	wp_register_script( 'moment', get_site_url() . '/app/libhours/js/vendor/moment.js', false, false, true );
 
-	wp_register_script( 'tabletop', '//' . $_SERVER['SERVER_NAME'] . '/app/libhours/js/vendor/tabletop.js', false, false, true );
+	wp_register_script( 'tabletop', get_site_url() . '/app/libhours/js/vendor/tabletop.js', false, false, true );
 
-	wp_register_script( 'underscore', '//' . $_SERVER['SERVER_NAME'] . '/app/libhours/js/vendor/underscore.js', false, false, true );
+	wp_register_script( 'underscore', get_site_url() . '/app/libhours/js/vendor/underscore.js', false, false, true );
 
-	wp_register_script( 'lib-hours', '//' . $_SERVER['SERVER_NAME'] . '/app/libhours/js/libhours.js', array( 'moment', 'tabletop', 'underscore' ), false, true );
+	wp_register_script( 'lib-hours', get_site_url() . '/app/libhours/js/libhours.js', array( 'moment', 'tabletop', 'underscore' ), false, true );
 
 	wp_register_script( 'forms', get_template_directory_uri() . '/js/login_functions.js', array(), '1.0.0', false );
 
