05e0f02ba826f23b01541988b3c751ddfb690c04
Matt Bernhardt <mjbernha@mit.edu>
2017-12-29 15:51:17 -0500

    Remove console logging

--- js/brand.js
+++ js/brand.js
@@ -1,32 +1,20 @@
 $(function(){
 
-	// Initialize Modernizr object
-	var Modernizr;
-
 	// What variable are we using to store toggle status?
-	var toggle = 'brand_closed_dev';
+	var toggle = 'brand_closed_test';
 
 	// This checks if the user has already closed the branding header.
 	// If localStorage
 	if (Modernizr.localstorage) {
-		console.log('Localstorage found');
 		// Check for the localStorage alert ID item
-		var status = localStorage.getItem(toggle);
 		if (localStorage.getItem(toggle) === 'true') {
-			console.log('Branding has already been closed');
 			$('.brand-splash').removeClass('big').addClass('compact');
-		} else {
-			console.log('Branding has not yet been closed');
 		}
-	} else { // No localStorage
-		console.log('No Localstorage found.');
 	}
 
 	// On click
 	$('.btn-minimize').click(function(){
-		console.log('Minimize branding');
 		$('.brand-splash').removeClass('big').addClass('compact');
-		// If localStorage:
 		if (Modernizr.localstorage) {
 			// Set the localStorage item, using the post ID
 			localStorage.setItem(toggle, 'true');
