2e1c96172c93660ac70f67b92af0c67e81175bc6
Matt Bernhardt <mjbernha@mit.edu>
2017-12-29 15:34:54 -0500

    Better debugging, abstraction

--- js/brand.js
+++ js/brand.js
@@ -1,13 +1,22 @@
 $(function(){
 
+	// Initialize Modernizr object
+	var Modernizr;
+
+	// What variable are we using to store toggle status?
+	var toggle = 'brand_closed_dev';
+
 	// This checks if the user has already closed the branding header.
 	// If localStorage
 	if (Modernizr.localstorage) {
 		console.log('Localstorage found');
 		// Check for the localStorage alert ID item
-		if (localStorage.getItem('brand_closed_dev') !== 'true') {
+		var status = localStorage.getItem(toggle);
+		if (localStorage.getItem(toggle) === 'true') {
 			console.log('Branding has already been closed');
 			$('.brand-splash').removeClass('big').addClass('compact');
+		} else {
+			console.log('Branding has not yet been closed');
 		}
 	} else { // No localStorage
 		console.log('No Localstorage found.');
@@ -20,7 +29,7 @@ $(function(){
 		// If localStorage:
 		if (Modernizr.localstorage) {
 			// Set the localStorage item, using the post ID
-			localStorage.setItem('brand_closed_dev', 'true');
+			localStorage.setItem(toggle, 'true');
 		}
 		// Prevent the default click behavior.
 		return false;
