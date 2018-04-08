cd4d1f84dd6a9f46a772167b4ef7054f78a33bb3
hpez <hpez58@gmail.com>
2018-04-08 15:44:03 +0430

    White space removed

--- functions.php
+++ functions.php
@@ -578,9 +578,9 @@ function getRoot( $post ) {
 	$ar = get_post_ancestors( $post );
 
 	$is_section = get_post_meta( $post->ID, 'is_section', 1 );
-	
+
 	$count_ar = count( $ar );
-	
+
 	for ( $i = 0;$i < $count_ar;$i++ ) {
 		$pid = $ar[ $i ];
 		$is_section = get_post_meta( $pid, 'is_section', 1 );
