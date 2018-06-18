2837d0b60ce07d9ff10f1cc9359118acdadd528b
Andy <dornera@mit.edu>
2018-06-18 09:08:47 -0400

    HTTPS Mixed-content NTI-356, NTI-357

--- header.php
+++ header.php
@@ -19,7 +19,7 @@
 <!--<meta name="viewport" content="width=device-width" />-->
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title><?php wp_title( '|', true, 'right' ); ?></title>
-<link rel="profile" href="http://gmpg.org/xfn/11" />
+<link rel="profile" href="https://gmpg.org/xfn/11" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 <?php wp_head(); ?>
 <?php
