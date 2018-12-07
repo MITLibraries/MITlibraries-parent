7f7c8019891f3d6e8e6d330a81d1cd8f5ed0a914
Frances Botsford <frances@switchingprotocols.com>
2018-12-07 13:46:41 -0500

    escape content on content-location

--- content-location.php
+++ content-location.php
@@ -93,11 +93,19 @@
 
 <div class="libraryAlertTop">
 <?php
-					include( locate_template( 'inc/alert.php' ) );
-					if ( $showAlert == 0 && $alertTitle != '' ) {
-						echo '<div class="libraryAlert">' . '<div class="location--alerts flex-container"><i class="icon-exclamation-sign"></i>' . '<div class="alertText">' . '<h3>' . $alertTitle . '</h3>' . '<p>' . $alertContent . '</p>' . '</div>' . '</div>' . '</div>';
-					}
-				?>
+include( locate_template( 'inc/alert.php' ) );
+if ( $showAlert === 0 && $alertTitle !== '' ) :
+?>
+	<div class="libraryAlert">
+		<div class="location--alerts flex-container">
+			<i class="icon-exclamation-sign"></i>
+			<div class="alertText">
+				<h3><?php echo esc_html( $alertTitle ); ?></h3>
+				<p><?php echo esc_html( $alertContent ); ?></p>
+			</div>
+		</div>
+	</div>
+<?php endif; ?>
 </div>				
 	<div class="title-page libraryTitle flex-container">
 		
@@ -107,21 +115,21 @@
 				<div class="libraryContent">
 					<h1>
 						<span class="libraryName"><?php the_title(); ?></span>
-						<span class="subject-library"><?php echo $subject ?></span>
+						<span class="subject-library"><?php echo esc_html( $subject ); ?></span>
 					</h1>
 					<div class="info-more">
-						<a href="tel:<?php echo $phone; ?>" class="phone"><?php echo $phone ?></a> |
+						<a href="tel:<?php echo esc_url( $phone ); ?>" class="phone"><?php echo esc_html( $phone ); ?></a> |
 							<?php if ( $email ) : ?>
-						<a href="mailto:<?php echo $email; ?>" class="email"><?php echo $email ?></a> |
+						<a href="mailto:<?php echo esc_url( $email ); ?>" class="email"><?php echo esc_html( $email ); ?></a> |
 							<?php endif; ?>
-						<a href="<?php echo $mapPage . $slug; ?>">Room: <?php echo $building ?> <i class="icon-arrow-right"></i></a>
+						<a href="<?php echo esc_url( $mapPage . $slug ); ?>">Room: <?php echo esc_html( $building ); ?> <i class="icon-arrow-right"></i></a>
 					</div>
 				</div><!-- end div.libraryContent -->
 
 				<div class="hours-today">
 					<span>Today's hours: <strong data-location-hours="<?php the_title(); ?>"></strong></span>
 					<?php if ( $study24 == 1 ) : ?>
-						| <a class="study-24-7" href="<?php echo $gStudy24Url; ?>" alt="This location contains one or more study spaces available 24 hours a day, seven days a week. Click the link for more info." title="Study 24/7">Study 24/7</a>
+						| <a class="study-24-7" href="<?php echo esc_url( $gStudy24Url ); ?>" alt="This location contains one or more study spaces available 24 hours a day, seven days a week. Click the link for more info." title="Study 24/7">Study 24/7</a>
 					<?php endif; ?>
 					<a href="/hours" class="link-hours-all">See all hours <i class="icon-arrow-right"></i></a>
 				</div><!-- end div.hours-today -->
@@ -134,14 +142,14 @@
 							$val = $arMain[ array_rand( $arMain ) ];
 						?>
 						<?php if ( $val != '' ) : ?>
-						<img src="<?php echo $val; ?>" data-thumb="<?php echo $val; ?>" alt="<?php the_title(); ?>" />
+						<img src="<?php echo esc_url( $val ); ?>" data-thumb="<?php echo esc_attr( $val ); ?>" alt="<?php the_title(); ?>" />
 						<?php endif; ?>
 				</div><!-- end div.library-image -->
 			</div><!-- end div.topRight -->
 		<!-- </div> end div.flex-item -->
 	</div><!-- end div.libraryTitle -->
 
-	<div id="content" class="content <?php echo $strLocation; ?> has-sidebar">
+	<div id="content" class="content <?php echo esc_attr( $strLocation ); ?> has-sidebar">
 		<div class="main-content content-main">
 
 			<?php
@@ -150,10 +158,10 @@
 			?>
 			<ul class="tabnav">
 				<?php if ( '' != $title1 ) : ?>
-				<li class="active tab1st"><h2 class="title-tab"><a href="#tab1"><?php echo $title1 ?><span class="title-sub hidden-mobile"><?php echo $subtitle1 ?></span class="title-sub"></a></h2></li>
+				<li class="active tab1st"><h2 class="title-tab"><a href="#tab1"><?php echo esc_html( $title1 ); ?><span class="title-sub hidden-mobile"><?php echo esc_html( $subtitle1 ); ?></span class="title-sub"></a></h2></li>
 				<?php endif; ?>
 				<?php if ( '' != $title2 ) : ?>
-				<li class="tab2nd"><h2 class="title-tab"><a href="#tab2"><?php echo $title2 ?><span class="title-sub hidden-mobile"><?php echo $subtitle2 ?></span class="title-sub"></a></h2></li>
+				<li class="tab2nd"><h2 class="title-tab"><a href="#tab2"><?php echo esc_html( $title2 ); ?><span class="title-sub hidden-mobile"><?php echo esc_html( $subtitle2 ); ?></span class="title-sub"></a></h2></li>
 				<?php endif; ?>
 			</ul>
 			<?php
@@ -162,7 +170,7 @@
 			endif;
 			?>
 
-			<div class="tabcontent group <?php echo $noTab ?>">
+			<div class="tabcontent group <?php echo esc_attr( $noTab ); ?>">
 
 				<div class="tab tab1 active flex-container group" id="tab1">
 
@@ -193,11 +201,11 @@
 								<div class="profile-content__body">
 									<h3>
 										<span class="intro">Featured expert</span>
-										<span class="name"><?php echo $name; ?></span>
-										<span class="bio"><?php echo $bio; ?></span>
+										<span class="name"><?php echo esc_html( $name ); ?></span>
+										<span class="bio"><?php echo esc_html( $bio ); ?></span>
 									</h3>
 									<div class="links">
-										<a class="primary" href="<?php echo $url; ?>" target="_blank">How can I help? <i class="icon-arrow-right"></i></a>
+										<a class="primary" href="<?php echo esc_url( $url ); ?>" target="_blank">How can I help? <i class="icon-arrow-right"></i></a>
 										<a href="/experts">See all our experts <i class="icon-arrow-right"></i></a>
 									</div>
 
@@ -213,7 +221,7 @@
 						</div>
 
 						<div class="flex-item second span3">
-							<?php echo $content1 ?>
+							<?php echo $content1; ?>
 						</div>
 
 				</div>
@@ -222,17 +230,17 @@
 				<div class="tab tab2 flex-container group" id="tab2">
 
 						<div class="flex-item first <?php if ( $content2wide ) : ?>span8 wideColumn<?php else : ?>span2<?php endif; ?>">
-						<?php echo $content2left ?>
+						<?php echo $content2left; ?>
 						
 						<?php if ( $reserveUrl != '' ) : ?>
-									<a class="reserve hidden-phone" href="<?php echo $reserveUrl; ?>"><?php echo $reserveText; ?></a>
+									<a class="reserve hidden-phone" href="<?php echo esc_url( $reserveUrl ); ?>"><?php echo esc_html( $reserveText ); ?></a>
 						<?php endif; ?>
 
 						
 						</div>
 
 						<div class="flex-item second span6">
-							<?php echo $content2 ?>
+							<?php echo $content2; ?>
 						</div>
 
 				</div>
