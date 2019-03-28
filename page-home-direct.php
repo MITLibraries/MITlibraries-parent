01d0acfde91b57c96f2c265e142694663996d8cd
Frances Botsford <frances@switchingprotocols.com>
2019-03-28 10:25:41 -0400

    add hayden reno notice to homepage and location page

--- page-home-direct.php
+++ page-home-direct.php
@@ -41,6 +41,10 @@ endif; ?>
 						<h3><a class="name-location" href="/hayden">Hayden Library</a></h3><div class="hours"><span data-location-hours="Hayden Library"></span> today,</div> <a href="/study/24x7/" class="special">24/7 Study</a><div class="location-info"><a href="/locations/#!hayden-library" class="map-location">14S-100</a><a href="tel:617-253-5671" class="phone"><span class="number">617-253-5671</span></a></div>
 					</div>
 				</div>
+				<div class="location-hayden-reno">
+					<p><a href="/future-spaces/">Upcoming Hayden renovation details</a>
+					</p>
+				</div>
 				<a href="#0" class="show-more hidden-non-mobile">
 					<svg class="icon-arrow-down" version="1.1" xmlns="http://www.w3.org/2000/svg" x="0" y="0" width="16.3" height="9.4" viewBox="2.7 8.3 16.3 9.4" enable-background="new 2.7 8.3 16.3 9.4" xml:space="preserve"><path d="M18.982 9.538l-8.159 8.159L2.665 9.538l1.284-1.283 6.875 6.875 6.875-6.875L18.982 9.538z"/></svg>Show 3 More
 				</a>
