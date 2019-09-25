688fc46cfa5745f14c6a17bdd4edfc1a2b16783a
Matt Bernhardt <mjbernha@mit.edu>
2019-09-25 12:49:10 -0400

    Replace Flickr icon with YouTube in social footer

--- footer.php
+++ footer.php
@@ -93,10 +93,10 @@
 						<span class="sr">RSS</span>
 					</a><!-- End RSS -->
 					
-					<a href="//www.flickr.com/photos/mit-libraries/" title="Flickr">
-						<svg class="icon-social--flickr" width="2048" height="2048" viewBox="-320 -384 2048 2048" enable-background="new 0 -128 1536 1536" xml:space="preserve"><path d="M698 640c0-58.7-20.7-108.7-62-150s-91.3-62-150-62 -108.7 20.7-150 62 -62 91.3-62 150 20.7 108.7 62 150 91.3 62 150 62 108.7-20.7 150-62S698 698.7 698 640zM1262 640c0-58.7-20.7-108.7-62-150s-91.3-62-150-62 -108.7 20.7-150 62 -62 91.3-62 150 20.7 108.7 62 150 91.3 62 150 62 108.7-20.7 150-62S1262 698.7 1262 640z"/></svg>
-						<span class="sr">Flickr</span>
-					</a><!-- End Flickr -->
+					<a href="//www.youtube.com/user/MITLibraries" title="YouTube">
+						<svg aria-hidden="true" focusable="false" data-prefix="fab" data-icon="youtube" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" class="icon-social--youtube"><path fill="black" d="M549.655 124.083c-6.281-23.65-24.787-42.276-48.284-48.597C458.781 64 288 64 288 64S117.22 64 74.629 75.486c-23.497 6.322-42.003 24.947-48.284 48.597-11.412 42.867-11.412 132.305-11.412 132.305s0 89.438 11.412 132.305c6.281 23.65 24.787 41.5 48.284 47.821C117.22 448 288 448 288 448s170.78 0 213.371-11.486c23.497-6.321 42.003-24.171 48.284-47.821 11.412-42.867 11.412-132.305 11.412-132.305s0-89.438-11.412-132.305zm-317.51 213.508V175.185l142.739 81.205-142.739 81.201z" class=""></path></svg>
+						<span class="sr">YouTube</span>
+					</a><!-- End YouTube -->
 					
 			</div><!-- end div.social -->
 			
