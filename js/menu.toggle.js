e2bd39d2ddffd724cc2bf1540eeda8c41f3ec8cc
Matt Bernhardt <mjbernha@mit.edu>
2019-11-27 15:27:16 -0500

    Adds alternative menu navigation for mobile screens; refactors javascript

--- js/menu.toggle.js
+++ js/menu.toggle.js
@@ -1,68 +1,58 @@
 $(function(){
-	$('#menu--toggle').click(function(){
-		$(this).toggleClass('active');
-		$(this).next().toggleClass('active');
-		console.log('clicked');
-		if ($(this).hasClass('active')) {
-			$(this).text('Hide Menu');
-		}
-		else {
-			$(this).text('View Menu');
-		}
-	});
+    // Activate hamburger menu on mobile displays.
+    // Why are there two of these toggles?
+    $('header .menu--toggle').click(function(){
+        $('#nav-main').toggleClass('active');
+        $('.wrap-page').toggleClass('mobile-nav-active');
+    });
+
+    // Show or hide the flyout menus on main navigation in response to
+    // mouseenter / mouseleave
+    $( '.link-primary' ).bind( "mouseenter", function() {
+        $( '.link-primary' ).removeClass( 'open' );
+        $(this).find( '.menu-control' ).attr( 'aria-expanded', 'true' );
+        $(this).closest( '.link-primary' ).addClass( 'open' );
+    });
+    $( '.link-primary' ).bind( "mouseleave", function() {
+        $(this).find( '.menu-control' ).attr( 'aria-expanded', 'false' );
+        $( '.link-primary' ).removeClass( 'open' );
+    });
+
+    // Make ESC close all menus.
+    // TODO: when the user has NVDA open, this will not work - but menus can
+    // still be closed by pressing "enter" on the parent item.
+    $( '#nav-main' ).on( 'keydown' , function(e) {
+        if (e.keyCode == 27) {
+            $( '.link-primary' ).removeClass( 'open' );
+            $( '.menu-control' ).attr('aria-expanded', 'false');
+            $( '.links-sub' ).attr( 'aria-hidden', 'true' );
+        }
+    });
+
 });
 
-// why are there two of these toggles?
-$('header .menu--toggle').click(function(){
-	$('#nav-main').toggleClass('active');
-	$('.wrap-page').toggleClass('mobile-nav-active');
-});
-
-
-$( '.link-primary' ).bind( "mouseenter", function() {	
-	$( '.link-primary' ).removeClass( 'open' );
-	$(this).find( '.menu-control' ).attr( 'aria-expanded', 'true' );
-	$(this).closest( '.link-primary' ).addClass( 'open' );
-});
-$( '.link-primary' ).bind( "mouseleave", function() {	
-	$(this).find( '.menu-control' ).attr( 'aria-expanded', 'false' );
-	$( '.link-primary' ).removeClass( 'open' );
-});
-
-// make esc close all menus
-$( '#nav-main' ).on( 'keydown' , function(e) {
-	if (e.keyCode == 27) {
-		hideMenu(e);
-	}
-});
-
-function hideMenu() {
-	$( '.link-primary' ).removeClass( 'open' );
-	$( '.menu-control' ).attr('aria-expanded', 'false');
-	$( '.links-sub' ).attr( 'aria-hidden', 'true' );
-}
-
+// This initializes the ARIA labels for the main navigation.
 // thanks to http://heydonworks.com/practical_aria_examples/
 $('.main-nav-header').each(function() {
 
-	var $this = $(this);
+    var $this = $(this);
 
-	// create unique id for a11y relationship
-	var id = 'collapsible-' + $( '#nav-main h2' ).index(this);
+    // create unique id for a11y relationship
+    var id = 'collapsible-' + $( '#nav-main h2' ).index(this);
 
-	// identify panel and make it focusable
-	var panel = $(this).next( '.links-sub' ).attr( 'aria-hidden', 'true' ).attr( 'id', id);
+    // identify panel and make it focusable
+    var panel = $(this).next( '.links-sub' ).attr( 'aria-hidden', 'true' ).attr( 'id', id);
 
-	// Add default aria states to button
-	$this.children( '.menu-control' ).attr( 'aria-expanded', 'false' ).attr( 'aria-controls', id);
-	var button = $this.children( '.menu-control' );
+    // Add default aria states to button
+    $this.children( '.menu-control' ).attr( 'aria-expanded', 'false' ).attr( 'aria-controls', id);
+    var button = $this.children( '.menu-control' );
 
-	// Toggle the state properties
-	button.on( 'click', function() {
-		$(this).closest( '.link-primary' ).toggleClass( 'open' );
-		var state = $(this).attr( 'aria-expanded' ) === 'false' ? true : false;
-		$(this).attr( 'aria-expanded', state );
-		panel.attr( 'aria-hidden', !state );
-	});
+    // Toggle the state properties
+    button.on( 'click', function() {
+        $(this).closest( '.link-primary' ).toggleClass( 'open' );
+        var state = $(this).attr( 'aria-expanded' ) === 'false' ? true : false;
+        $(this).attr( 'aria-expanded', state );
+        panel.attr( 'aria-hidden', !state );
+    });
 
 });
