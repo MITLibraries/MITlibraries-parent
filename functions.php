32ee05f411fbe3750c08b680a6dc46bf694eae03
Matt Bernhardt <mjbernha@mit.edu>
2020-09-09 17:34:56 -0400

    Making PHPCS and CodeClimate happy

--- functions.php
+++ functions.php
@@ -159,7 +159,7 @@ function twentytwelve_scripts_styles() {
 
 	wp_register_script( 'productionJS', get_template_directory_uri() . '/js/build/production.min.js', array( 'jquery', 'moment', 'underscore' ), '1.10.0', true );
 
-	wp_register_script( 'hours-loader', get_template_directory_uri() . '/js/hours-loader.js', array( 'moment', 'underscore'), '1.10.0', true);
+	wp_register_script( 'hours-loader', get_template_directory_uri() . '/js/hours-loader.js', array( 'moment', 'underscore' ), '1.10.0', true );
 
 	wp_register_script( 'hours-gldatepickerJS', get_template_directory_uri() . '/libs/datepicker/glDatePicker.min.js', false, null, true );
 
