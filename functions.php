a212495dc3290986b3a27f84e3542ebb1f16d150
Matt Bernhardt <mjbernha@mit.edu>
2022-05-03 16:16:27 -0400

    Convert content below hours grid to a widget area
    ** Why are these changes being introduced:
    
    * As the Libraries have re-opened, there have been a number of changes
      to the currently-static-HTML content below the hours grid. In order to
      allow us to change this content more responsively as the organization
      evolves, this converts that static content into a widget area so that
      changes don't require code updates.
    
    ** Relevant ticket(s):
    
    * https://mitlibraries.atlassian.net/browse/uxws-1367
    
    ** How does this address that need:
    
    * This takes out the static content from page-hours-json.php and
      replaces it with a new sidebar area. As part of the deploy of this, I
      will create and populate a widget with the static content.
    
    ** Document any side effects to this change:
    
    * The actual markup around this content will change, but not in a way
      that will be visible to the user or a search engine.

--- functions.php
+++ functions.php
@@ -353,6 +353,15 @@ function mitlib_widgets_init() {
 		'after_title'   => '</h2>',
 	) );
 
+	register_sidebar( array(
+		'name'          => __( 'Below Hours Grid Area', 'twentytwelve' ),
+		'id'            => 'sidebar-below-hours-grid',
+		'description'   => __( 'Appears below the grid of library hours, allowing for widgets to describe locations that do not appear on the grid.', 'twentytwelve' ),
+		'before_widget' => '<aside id="%1$s" class="widget %2$s locations-more" role="complementary">',
+		'after_widget'  => '</aside>',
+		'before_title'  => '<h3 class="widget-title">',
+		'after_title'   => '</h3>',
+	) );
 }
 add_action( 'widgets_init', 'mitlib_widgets_init' );
 
