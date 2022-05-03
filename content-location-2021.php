f0d5ccdf118c10010427337ead470db238662383
Matt Bernhardt <mjbernha@mit.edu>
2022-05-03 13:56:01 -0400

    More permissive check for showing location alerts
    ** Why are these changes being introduced:
    
    * The most recent location template - introduced last fall - uses too
      strict a comparison to determine whether a location alert will be
      shown - the === operator used is different from that used by the older
      location template (which uses ==). I didn't realize initially the
      using the stricter comparison would never result in the alert being
      shown, it seems.
    
    ** Relevant ticket(s):
    
    * https://mitlibraries.atlassian.net/browse/uxws-1368
    
    ** How does this address that need:
    
    * This change reverts the new location template to using the more
      permissive == operator instead of ===.
    
    ** Document any side effects to this change:
    
    * I'm still not entirely sure about how the logic of this check works,
      so there may be some aspect - but I doubt it. This reverts to the same
      comparison that is working for the other location template.

--- content-location-2021.php
+++ content-location-2021.php
@@ -71,7 +71,7 @@ $alertContent = cf( 'alert_content' );
 <div class="libraryAlertTop">
 	<?php
 	require locate_template( 'inc/alert.php' );
-	if ( 0 === $showAlert && '' !== $alertTitle ) {
+	if ( 0 == $showAlert && '' !== $alertTitle ) {
 		echo '<div class="libraryAlert">' . '<div class="location--alerts flex-container"><i class="icon-exclamation-sign"></i>' . '<div class="alertText">' . '<h3>' . $alertTitle . '</h3>' . '<p>' . $alertContent . '</p>' . '</div>' . '</div>' . '</div>';
 	}
 	?>
