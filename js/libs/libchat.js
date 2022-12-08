ca550e74c42b778f6f9a427fd0a0a085bc68389b
Matt Bernhardt <mjbernha@mit.edu>
2022-12-08 14:48:39 -0500

    Update libchat.js to use new URL format
    ** Why are these changes being introduced:
    
    * Springshare is deprecating the URL pattern we have used previously
      when loading their javascript-based chat integration. The new URL
      pattern makes use of our custom doman name, rather than a generic
      domain across all customers.
    
    ** Relevant ticket(s):
    
    * https://mitlibraries.atlassian.net/browse/uxws-1423
    
    ** How does this address that need:
    
    * This updates libchat.js in the parent theme to use the new URL pattern
    * The theme version is bumped because of this change, to 1.13.2.
    
    ** Document any side effects to this change:
    
    * I'm choosing to include the https protocol now, rather than the
      protocol-agnostic `//` prefix. The Springshare documentation uses
      the protocol in their examples, and it seems safe to explicitly call
      for https at this point.

--- js/libs/libchat.js
+++ js/libs/libchat.js
@@ -1,3 +1,3 @@
 $(function(){
-   $.getScript('//v2.libanswers.com/load_chat.php?hash=be2c654b63dd43f31c56295ee5d78d88');
+   $.getScript('https://libanswers.mit.edu/load_chat.php?hash=be2c654b63dd43f31c56295ee5d78d88');
 });
